7a6e0886e064d3bda65bc01f30ff7f80235f5660
David Richardson <davidr@ebi.ac.uk>
2017-09-21 15:37:27 +0100

    externalise config

--- src/main/java/uk/ac/ebi/subs/biostudies/client/BioStudiesConfig.java
+++ src/main/java/uk/ac/ebi/subs/biostudies/client/BioStudiesConfig.java
@@ -2,15 +2,17 @@ package uk.ac.ebi.subs.biostudies.client;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.net.URI;
 
+
 @Data
 @ConfigurationProperties("usi.biostudies")
+@Component
 public class BioStudiesConfig {
     private String server;
     private Auth auth = new Auth();
-
 
     @Data
     public static class Auth {
--- src/test/java/uk/ac/ebi/subs/biostudies/client/BioStudiesClientTest.java
+++ src/test/java/uk/ac/ebi/subs/biostudies/client/BioStudiesClientTest.java
@@ -4,34 +4,25 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.subs.biostudies.BioStudiesAgentApp;
 import uk.ac.ebi.subs.biostudies.TestUtil;
 import uk.ac.ebi.subs.biostudies.model.BioStudiesSubmission;
-import uk.ac.ebi.subs.biostudies.model.BioStudiesSubsection;
 
 import java.util.UUID;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-public class EstablishSession {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = BioStudiesAgentApp.class)
+public class BioStudiesClientTest {
 
+    @Autowired
     private BioStudiesConfig config;
     private BioStudiesSubmission bioStudiesSubmission;
 
-    @Value("${biostudiesUser}")
-    private String biostudiesUser = "davidr@ebi.ac.uk";
-    @Value("${biostudiesPassword}")
-    private String biostudiesPassword = "hellougis";
-    @Value("${biostudiesServer}")
-    private String biostudiesServer = "http://biostudy-dev.ebi.ac.uk:10180/biostd-beta";
-
-
     @Before
     public void buildup() {
-        config = new BioStudiesConfig();
-        config.setServer(biostudiesServer);
-        config.getAuth().setLogin(biostudiesUser);
-        config.getAuth().setPassword(biostudiesPassword);
 
         bioStudiesSubmission = (BioStudiesSubmission) TestUtil.loadObjectFromJson(
                 "exampleProject_biostudies.json", BioStudiesSubmission.class
@@ -50,10 +41,13 @@ public class EstablishSession {
 
     @Test(expected = IllegalArgumentException.class)
     public void loginFailure() {
-        config.getAuth().setLogin(UUID.randomUUID().toString());
-        config.getAuth().setPassword(UUID.randomUUID().toString());
+        BioStudiesConfig badConfig = new BioStudiesConfig();
+        badConfig.setServer(config.getServer());
 
-        BioStudiesClient client = new BioStudiesClient(config);
+        badConfig.getAuth().setLogin(UUID.randomUUID().toString());
+        badConfig.getAuth().setPassword(UUID.randomUUID().toString());
+
+        BioStudiesClient client = new BioStudiesClient(badConfig);
         BioStudiesSession session = client.initialiseSession();
     }
 
@@ -62,22 +56,13 @@ public class EstablishSession {
         BioStudiesClient client = new BioStudiesClient(config);
         BioStudiesSession session = client.initialiseSession();
 
-        int sectCounter = 0;
-
-        bioStudiesSubmission.setAccno("!{SUBSPRJ}");
-        bioStudiesSubmission.getSection().setAccno("PROJECT");
-        for (BioStudiesSubsection subsection : bioStudiesSubmission.getSection().getSubsections()){
-            if (!subsection.isAccessioned()){
-                sectCounter++;
-                subsection.setAccno("SECT" + sectCounter);
-            }
-        }
-
         SubmissionReport response = session.submit(bioStudiesSubmission);
 
-        Assert.assertEquals("SUCCESS",response.getStatus());
+        Assert.assertEquals("OK", response.getStatus());
         Assert.assertNotNull(response.findAccession());
         Assert.assertTrue(response.findAccession().startsWith("SUBSPRJ"));
+
+        System.out.println(response.findAccession());
 
     }
 
