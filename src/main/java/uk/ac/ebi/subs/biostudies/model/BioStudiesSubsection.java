24e55af868b8a4be75fa7a2c95b3ecad667c603d
David Richardson <davidr@ebi.ac.uk>
2017-10-27 12:04:43 +0100

    add mock tests for project processor

--- src/main/java/uk/ac/ebi/subs/biostudies/model/BioStudiesSubsection.java
+++ src/main/java/uk/ac/ebi/subs/biostudies/model/BioStudiesSubsection.java
@@ -3,13 +3,10 @@ package uk.ac.ebi.subs.biostudies.model;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Data
-public class BioStudiesSubsection implements BioStudiesAccessioned{
+public class BioStudiesSubsection implements BioStudiesAccessioned {
     private String type;
     private String accno;
     private List<BioStudiesAttribute> attributes = new ArrayList<>();
