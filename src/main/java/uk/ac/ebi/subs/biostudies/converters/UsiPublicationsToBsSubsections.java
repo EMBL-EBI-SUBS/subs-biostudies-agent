1eb0c4bb04bd2e07ffa1875f4d1649f31dc8c84a
David Richardson <davidr@ebi.ac.uk>
2018-11-23 14:52:49 +0000

    update publication mapping

--- src/main/java/uk/ac/ebi/subs/biostudies/converters/UsiPublicationsToBsSubsections.java
+++ src/main/java/uk/ac/ebi/subs/biostudies/converters/UsiPublicationsToBsSubsections.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import uk.ac.ebi.subs.biostudies.model.BioStudiesAttribute;
 import uk.ac.ebi.subs.biostudies.model.BioStudiesSubsection;
 import uk.ac.ebi.subs.data.component.Publication;
+import uk.ac.ebi.subs.data.component.PublicationStatus;
 import uk.ac.ebi.subs.data.component.Publications;
 
 import java.util.List;
@@ -46,6 +47,47 @@ public class UsiPublicationsToBsSubsections implements Converter<Publications, L
                     BioStudiesAttribute.builder().name("pubmedId").value(publication.getPubmedId()).build()
             );
             accno = pubmedIdNumericPart(publication.getPubmedId());
+        }
+
+        if (publication.getArticleTitle() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("articleTitle").value(publication.getArticleTitle()).build()
+            );
+        }
+        if (publication.getJournalTitle() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("journalTitle").value(publication.getJournalTitle()).build()
+            );
+        }
+        if (publication.getAuthors() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("authors").value(publication.getAuthors()).build()
+            );
+        }
+        if (publication.getJournalIssn() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("journalIssn").value(publication.getJournalIssn()).build()
+            );
+        }
+        if (publication.getIssue() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("issue").value(publication.getIssue()).build()
+            );
+        }
+        if (publication.getYear() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("year").value(publication.getYear()).build()
+            );
+        }
+        if (publication.getVolume() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("volume").value(publication.getVolume()).build()
+            );
+        }
+        if (publication.getPageInfo() != null){
+            subsection.getAttributes().add(
+                    BioStudiesAttribute.builder().name("pageInfo").value(publication.getPageInfo()).build()
+            );
         }
 
         subsection.setAccno(accno);
